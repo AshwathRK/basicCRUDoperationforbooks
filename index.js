c157a32ef51fb37d2a29179b014d14f59444b744
Ashwathaman <ashwathaman.r6@gmail.com>
2025-05-08 09:47:44 +0530

    view added

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const { log } = require('console');
 const express = require('express');
 const HTTP_SERVER = express();
 const { logReqResponceMiddleware } = require('./Middlewares/loging');
-const booksRouter = require('./Router/router.books')
+const booksRouter = require('./Router/router.books');
+const path = require('path');
 
 HTTP_SERVER.use(express.json()); // Enables JSON body parsing
 HTTP_SERVER.use(logReqResponceMiddleware); // Log Middleware
 HTTP_SERVER.use('/', booksRouter)
+
+HTTP_SERVER.use('/home', (req, res, next)=>{
+    res.sendFile(path.resolve('./home'))
+})
 
 
 // Start server
